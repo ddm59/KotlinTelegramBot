e609bce879c8624ce470c20a3a7b8278dbee5d8b
ddm59 <ddm591155@gmail.com>
2025-02-05 13:44:59 +0600

    KTB-05 console menu realization

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -2,9 +2,45 @@ package additional
 
 import java.io.File
 
-fun main() {
+const val DICTIONARY_FILE_PATH = "words.txt"
+const val MENU_ITEMS = """
+    Меню: 
+    1 – Учить слова
+    2 – Статистика
+    0 – Выход
+"""
 
-    val wordsFile: File = File("words.txt")
+fun main() {
+    var isExit: Boolean = false
+    val dictionary: MutableList<Word> = loadDictionary()
+
+    while (!isExit) {
+
+        println(MENU_ITEMS.trimIndent())
+
+        val userInput = readln().toIntOrNull() ?: 0
+
+        when (userInput) {
+            1 -> {
+                println("Выбран пункт меню: 1 - Учить слова")
+            }
+
+            2 -> {
+                println("Выбран пункт меню: 2 - Статистика")
+            }
+
+            0 -> {
+                println("Выход из меню")
+                isExit = true
+            }
+
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
+}
+
+fun loadDictionary(): MutableList<Word> {
+    val wordsFile: File = File(DICTIONARY_FILE_PATH)
     val dictionary: MutableList<Word> = mutableListOf()
     val lines: List<String> = wordsFile.readLines()
 
@@ -13,5 +49,5 @@ fun main() {
         val word = Word(word = line[0], translate = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
         dictionary.add(word)
     }
-    dictionary.forEach { println(it) }
+    return dictionary
 }
