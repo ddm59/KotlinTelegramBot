ba38703192db7652217f1c3559253fddf0dc895f
ddm59 <ddm591155@gmail.com>
2025-03-02 13:41:50 +0600

    KTB-16 telegram bot buttons and OkHttp

--- src/main/kotlin/additional/TelegramBotService.kt
+++ src/main/kotlin/additional/TelegramBotService.kt
@@ -1,33 +1,64 @@
 package additional
 
-import java.net.URI
+import okhttp3.MediaType.Companion.toMediaType
 import java.net.URLEncoder
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
 
-const val  TG_BOT_URL = "https://api.telegram.org/bot"
 
+const val TG_BOT_URL = "https://api.telegram.org/bot"
 
 class TelegramBotService(
     private val botToken: String,
 ) {
-    private val client: HttpClient = HttpClient.newBuilder().build()
+    private val  client: OkHttpClient = OkHttpClient()
 
     fun getUpdates(updateId: Int): String {
         val urlGetUpdates = "$TG_BOT_URL$botToken/getUpdates?offset=$updateId"
-        val request = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        return response.body()
+        val request = Request.Builder().url(urlGetUpdates).build()
+
+        client.newCall(request).execute().use { response: Response ->
+            return response.body?.string() ?: ""
+        }
     }
 
     fun sendMessage(chatId: Long, message: String): String {
         val encodedMessage = URLEncoder.encode(message, Charsets.UTF_8.toString())
-        val sendMessage =
-            "$TG_BOT_URL$botToken/sendMessage?chat_id=$chatId&text=$encodedMessage"
-        val request = HttpRequest.newBuilder().uri(URI.create(sendMessage)).build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        return response.body()
+        val url = "$TG_BOT_URL$botToken/sendMessage?chat_id=$chatId&text=$encodedMessage"
+
+        val request = Request.Builder().url(url).build()
+        client.newCall(request).execute().use { response: Response ->
+            return response.body?.string() ?: ""
+        }
     }
 
+    fun sendMenu(chatId: Long): String {
+        val url = "$TG_BOT_URL$botToken/sendMessage"
+        val jsonBody = """{
+        "chat_id": $chatId,
+        "text": "Основное меню",
+        "reply_markup": {
+            "inline_keyboard": [
+                [
+                    {"text": "Изучить слова", "callback_data": "learn_words_clicked"},
+                    {"text": "Статистика", "callback_data": "statistics_clicked"}
+                ]
+            ]
+        }
+    }""".trimIndent()
+
+        val requestBody = RequestBody.create("application/json".toMediaType(), jsonBody)
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .header("Content-Type", "application/json")
+            .build()
+
+        client.newCall(request).execute().use { response: Response ->
+            return response.body?.string() ?: ""
+        }
+    }
 }
